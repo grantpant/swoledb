6d8b50e77a8129cc787f88e6663d622b23ec7b93
grantpant <grantpant@gmail.com>
2019-05-24 21:25:43 -0500

    Configure Apollo Client devtools

--- src/app.js
+++ src/app.js
@@ -6,8 +6,6 @@ import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
-console.log('GRAPHQL_ENDPOINT = ', process.env.GRAPHQL_ENDPOINT);
-
 export const client = new ApolloClient({
   uri: process.env.GRAPHQL_ENDPOINT,
   request: (operation) => {
@@ -16,7 +14,8 @@ export const client = new ApolloClient({
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJzd29sZWRiQGRldiIsInJvbGVzIjpbImFkbWluIl19LCJpYXQiOjE1NTc4NDkyODksImV4cCI6MTU1ODQ1NDA4OX0.yFp_azTalEUltAl9QUJjvCm7EMZCVUDImC7n-IKOH4U'
       }
     });
-  }
+  },
+  connectToDevTools: true
 });
 
 ReactDOM.render(
