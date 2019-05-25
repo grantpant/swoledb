3e9cb434f5fbec8c78ea8c0c182310271c5ea7a7
grantpant <grantpant@gmail.com>
2019-05-24 22:35:33 -0500

    Add icon to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,11 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header className="header">
     <div className="container header__container">
-      <h1 className="header__title">SwoleDB</h1>
+      <div className="header__title">
+        <img src="/images/swoledb.png" className="header__title__icon"/>
+        <h1 className="header__title__text">SwoleDB</h1>
+        <img src="/images/swoledb.png" className="header__title__icon"/>
+      </div>
       <div className="header__nav">
         <NavLink exact to="/" className="header__nav__link" activeClassName="is-active">Add Exercise</NavLink>
         <NavLink to="/search" className="header__nav__link" activeClassName="is-active">Search Exercises</NavLink>
