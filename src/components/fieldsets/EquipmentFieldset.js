5c3b4a438d0d3ae5252b9c27f6a738d1740387e4
grantpant <grantpant@gmail.com>
2019-05-29 09:08:38 -0500

    CamelCase values from checkbox fieldsets going to inputs

--- src/components/fieldsets/EquipmentFieldset.js
+++ src/components/fieldsets/EquipmentFieldset.js
@@ -3,24 +3,32 @@ import EquipmentPieceInput from './inputs/EquipmentPieceInput';
 
 const equipment = ['Barbell', 'Barbell rack', 'Dumbbells', 'Bench', 'Kettle bell', 'Smith rack', 'Cable', 'Dip station', 'Roman chair', 'Pull-up bar', 'TRX straps', 'Step', 'Box', 'Band', 'Medicine ball', 'Swiss ball', 'BOSU ball', 'Foam roller', 'Cones', 'Agility ladder', 'Mini hurdles'];
 
+const toCamelCase = (equipmentPiece) => {
+  // It hasn't been lower-cased yet if coming from the rendered list.
+  equipmentPiece = equipmentPiece.toLowerCase();
+  // Check if the equipmentPiece has a space or a hyphen in it
+  const hasSpace = new RegExp(/\s/, 'g').test(equipmentPiece);
+  const hasHyphen = new RegExp(/(-[\w])/, 'g').test(equipmentPiece);
+
+  if (hasSpace) {
+    equipmentPiece = equipmentPiece.replace(/(\s[\w])/g, (match) => (
+      match[1].toUpperCase()
+    ));
+  }
+  if (hasHyphen) {
+    equipmentPiece = equipmentPiece.replace(/(-[\w])/g, (match) => (
+      match[1].toUpperCase()
+    ));
+  }
+
+  return equipmentPiece;
+};
+
 class EquipmentFieldset extends React.Component {
   onChange = (e) => {
     let equipmentPiece = e.target.value;
 
-    // Check if the equipmentPiece has a space or a hyphen in it
-    const hasSpace = new RegExp(/\s/, 'g').test(equipmentPiece);
-    const hasHyphen = new RegExp(/(-[\w])/, 'g').test(equipmentPiece);
-
-    if (hasSpace) {
-      equipmentPiece = equipmentPiece.replace(/(\s[\w])/g, (match) => (
-        match[1].toUpperCase()
-      ));
-    }
-    if (hasHyphen) {
-      equipmentPiece = equipmentPiece.replace(/(-[\w])/g, (match) => (
-        match[1].toUpperCase()
-      ));
-    }
+    equipmentPiece = toCamelCase(equipmentPiece);
 
     this.props.onChange(equipmentPiece);
   };
@@ -31,14 +39,18 @@ class EquipmentFieldset extends React.Component {
           <h4>Equipment</h4>
         </div>
         <div className="fieldset__body">
-          {equipment.map((equipmentPiece, i) => (
-            <EquipmentPieceInput
-              key={i}
-              equipmentPiece={equipmentPiece}
-              checked={this.props.state[equipmentPiece]}
-              onChange={this.onChange}
-            />
-          ))}
+          {equipment.map((equipmentPiece, i) => {
+            const checkedValue = toCamelCase(equipmentPiece);
+
+            return (
+              <EquipmentPieceInput
+                key={i}
+                equipmentPiece={equipmentPiece}
+                checked={this.props.state[checkedValue]}
+                onChange={this.onChange}
+              />
+            );
+          })}
         </div>
       </fieldset>
     );
--- src/components/fieldsets/WorkoutTypesFieldset.js
+++ src/components/fieldsets/WorkoutTypesFieldset.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import WorkoutTypeInput from './inputs/WorkoutTypeInput';
 
 const workoutTypes = ['Warm-up', 'Resistence', 'H.I.I.T.'];
 
-class WorkoutTypesFieldset extends React.Component {
+const toCamelCase = (workoutType) => {
+  // It hasn't been "toLowerCased" yet if coming from the render method
+  workoutType = workoutType.toLowerCase();
+  const hasHyphen = new RegExp(/(-[\w])/, 'g').test(workoutType);
+
+  if (hasHyphen) {
+    workoutType = workoutType.replace(/(-[\w])/g, (match) => (
+      match[1].toUpperCase()
+    ));
+  }
+  if (workoutType === 'h.i.i.t.') {
+    workoutType = workoutType.replace(/\./g,'');
+  }
+
+  return workoutType;
+};
+
+class WorkoutTypesFieldset extends Component {
   onChange = (e) => {
     let workoutType = e.target.value;
-    const hasHyphen = new RegExp(/(-[\w])/, 'g').test(workoutType);
 
-    if (hasHyphen) {
-      workoutType = workoutType.replace(/(-[\w])/g, (match) => (
-        match[1].toUpperCase()
-      ));
-    }
-    if (workoutType === 'h.i.i.t.') {
-      workoutType = workoutType.replace(/\./g,'');
-    }
+    workoutType = toCamelCase(workoutType);
 
     this.props.onChange(workoutType);
   };
@@ -26,14 +35,18 @@ class WorkoutTypesFieldset extends React.Component {
           <h4>Workout Types</h4>
         </div>
         <div className="fieldset__body">
-          {workoutTypes.map((workoutType, i) => (
-            <WorkoutTypeInput
-              key={i}
-              workoutType={workoutType}
-              checked={this.props.state[workoutType]}
-              onChange={this.onChange}
-            />
-          ))}
+          {workoutTypes.map((workoutType, i) => {
+            const checkedValue = toCamelCase(workoutType);
+
+            return (
+              <WorkoutTypeInput
+                key={i}
+                workoutType={workoutType}
+                checked={this.props.state[checkedValue]}
+                onChange={this.onChange}
+              />
+            );
+          })}
         </div>
       </fieldset>
     );
