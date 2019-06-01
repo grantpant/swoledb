d578748f254fe09b448406560191d2b4101e5752
grantpant <grantpant@gmail.com>
2019-06-01 13:08:12 -0500

    Add desktop breakpoint to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,10 @@ import { history } from '../routers/AppRouter';
 
 const Header = () => (
   <header className="header">
-    <div className="container header__container">
+    <div className="header__container">
       <div className="header__title">
         <img src="/images/swoledb.png" className="header__title__icon"/>
         <h1 className="header__title__text">SwoleDB</h1>
-        <img src="/images/swoledb.png" className="header__title__icon"/>
       </div>
       <div className="header__nav">
         <Query query={GET_LOGGED_IN_STATUS}>
@@ -22,8 +21,8 @@ const Header = () => (
             return (
               data.isLoggedIn && (
                 <Fragment>
+                  <NavLink to="/dashboard" className="header__nav__link" activeClassName="is-active">Exercise Search</NavLink>
                   <NavLink exact to="/add-exercise" className="header__nav__link" activeClassName="is-active">Add Exercise</NavLink>
-                  <NavLink to="/dashboard" className="header__nav__link" activeClassName="is-active">Search Exercises</NavLink>
                   <button
                     className="button header__nav__button"
                     onClick={() => {
